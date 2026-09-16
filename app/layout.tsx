ca7cd0d45485e32a70b2f5fa27f5755670da8cf8
Lora Digital <loradigital@Lora-MacBook-Neo.local>
2026-09-16 05:13:28 -0700

    Tum sayfalara WhatsApp butonu eklendi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import WhatsAppButton from "./components/WhatsAppButton";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -23,7 +24,8 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">{children}  <WhatsAppButton />
+      </body>
     </html>
   );
 }
